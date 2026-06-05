ddc4f2a5d4a1b12a57fa16de5c590261be9a367e
ArumugaSelvam <arumugaselvam23052003@gmail.com>
2026-06-05 16:16:24 +0530

    baseURL issue resolved

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,12 @@
 //centeralized API setup
 
-import axios from 'axios';
-import qs from 'qs';
+import axios from "axios";
+import qs from "qs";
 
 const api = axios.create({
-    baseURL: '/api',
-    withCredentials: true,
-    paramsSerializer: params => qs.stringify(params, { arrayFormat: 'repeat' }),
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+  paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
+});
 
 export default api;
