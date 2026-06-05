6079a831765172745c904e4d2371514ac32bea76
ArumugaSelvam <arumugaselvam23052003@gmail.com>
2026-06-05 16:22:52 +0530

    baseURL issue resolved with api word

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import qs from "qs";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL + "/api",
   withCredentials: true,
   paramsSerializer: (params) => qs.stringify(params, { arrayFormat: "repeat" }),
 });
